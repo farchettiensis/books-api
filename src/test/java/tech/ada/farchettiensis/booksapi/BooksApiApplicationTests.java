bcce5509bed1cb6c8d60602a753c5005ad24708d
farchettiensis <farchettiensis@users.noreply.github.com>
2024-10-23 23:18:40 -0300

    Initialize spring application

---
+++ src/test/java/tech/ada/farchettiensis/booksapi/BooksApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.ada.farchettiensis.booksapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
